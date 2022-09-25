724c8423cefc2dee7707dcae24001feb7d3718c5
Benjamin <benver17@gmail.com>
2022-09-25 18:10:01 -0500

    /:movieId/theaters route to movies.router

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,5 +1,5 @@
 const service = require("./movies.service");
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res, next) {
   const data = await service.list(req.query.is_showing);
@@ -24,5 +24,5 @@ async function read(req, res, next) {
 module.exports = {
   list: asyncErrorBoundary(list),
   read: [asyncErrorBoundary(movieExsists), read],
-  
+
 }
