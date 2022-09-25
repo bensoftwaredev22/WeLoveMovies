4f738b18ee1bb304869e6708c6c48eed2bf08845
Benjamin <benver17@gmail.com>
2022-09-25 18:33:39 -0500

    added /:movieId/reviews router to movies.router

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -33,7 +33,7 @@ function theaterList(movieId) {
 function read(movieId) {
   return knex('movies')
     .select('*')
-    .where({ movie_id: movieId })
+    .where({ movie_Id: movieId })
     .first();
 }
 
