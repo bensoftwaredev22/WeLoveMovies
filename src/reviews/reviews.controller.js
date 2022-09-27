b5bdc01807a46b13a6bea3fbbe2ebe454dcf3eb2
Benjamin <benver17@gmail.com>
2022-09-26 21:36:41 -0500

    added funtions to reviews files

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,5 +1,24 @@
-/*
 const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-*/
+
+async function reviewExsists(req, res, next) {
+    const { reviewId } = req.params;
+
+    const review = await service.read(reviewId)
+
+    if (review) {
+        res.locals.review = review;
+        //console.log(review)
+        return next();
+    }
+    next({ status: 404, message: "Review cannot be found."})
+}
+
+async function update(req, res, next) {
+    res.json({ data: await service.update(req.body) })
+}
+
+module.exports = {
+ update: [asyncErrorBoundary(reviewExsists), asyncErrorBoundary(update)],
+}
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,10 +1,21 @@
-/*
-
 const knex = require("../db/connection");
 
+function read(reviewId) {
+    return knex("reviews").select("*").where({review_id: reviewId }).first();
+}
+
+function update(updatedReview) {
+    const updatedReviewResponse =  knex("reviews")
+        .select('*')
+        .where({ review_id: reviewId })
+        .update(updatedReview, '*');
+
+        return updatedReviewResponse[0]
+}
 
 module.exports = {
-  
+    read,
+    update,
+
 };
 
-*/
